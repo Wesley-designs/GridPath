f8825274e6f4f99cdcacd4ee844fc272f567a6af
Wesley-designs <jiewesleyd2789@nycstudents.net>
2024-10-30 13:33:22 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Location a = new Location(0, 0);
+        System.out.println(a.getCol());
+        System.out.println(a.getRow());
+    }
+}
