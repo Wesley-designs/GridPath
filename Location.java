78235f97f577ad6281a773ac90d1ee67c0077839
Wesley-designs <jiewesleyd2789@nycstudents.net>
2024-11-12 14:33:21 +0000

    e

--- Location.java
+++ Location.java
@@ -1,15 +1,22 @@
-public class Location
-{
+public class Location {
     private int theRow;
     private int theCol;
-public Location(int r, int c)
-    {
+
+    public Location(int r, int c) {
         theRow = r;
         theCol = c;
     }
-public int getRow()
-    { return theRow; }
 
-public int getCol()
-    { return theCol; }
+    public int getRow() {
+        return theRow;
+    }
+
+    public int getCol() {
+        return theCol;
+    }
+
+    public String toString() {
+        return theRow + " " + theCol;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Location test = new Location(1, 1);
-        GridPath a = new .getNextLoc(1,1);
+
         System.out.println(test);
+
+        GridPath g = new GridPath();
+        System.out.println(g.getNextLoc(0, 0));
     }
 }
